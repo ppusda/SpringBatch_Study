e0c94f7e7fa3890b6bbe4e7f4f0b9602d5b629a8
ppusda <ppusda1234@gmail.com>
2024-01-11 13:57:09 +0900

    feat: git init

---
+++ src/main/java/com/example/springbatch_study/SpringBatchStudyApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbatch_study;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStudyApplication.class, args);
+    }
+
+}
+
+
